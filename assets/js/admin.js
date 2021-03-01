fcbc78df76645ccb8707fe77841269e75b3c5e5b
moez87 <harrabimoez87@gmail.com>
2021-03-01 15:17:14 +0100

    fix affichage resevation

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -13,13 +13,13 @@ function addmatch(){
         match +=`
             <div class="col-lg-4 col-md-6 mt-md-0 mt-5">
                 <div class="grids5-info">
-                    <span class="posted-date">
-                        <a href="#blog-single">
-                        <span class="small">${x.date[0]+x.date[1]+x.date[2]+x.date[3]}</span>
-                        <span class="big">${x.date[8]+x.date[9]}</span>
-                        <span class="small">${x.date[5]+x.date[6]}</span>
-                        </a>
-                    </span>
+                <span class="posted-date">
+                <a href="#blog-single">
+                <span class="small">${x.date[0]+x.date[1]+x.date[2]+x.date[3]}</span>
+                <span class="big">${x.date[8]+x.date[9]}</span>
+                <span class="small">${x.date[5]+x.date[6]}</span>
+                </a>
+                   </span>
                     <a href="reservation.html" class="d-block zoom"><img src="assets/images/1200px-Dean_Smith_Center1.jpg" alt=""
                     class="img-fluid news-image" /></a>
                     <div class="blog-info">
