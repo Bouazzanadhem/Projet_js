96880cba440da1ab0a821b09a498161ddd299fcc
Bouazzanadhem <bouazzanadhem@gmail.com>
2021-03-01 23:02:00 +0100

    simple change not fixed

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,28 @@
-function login() {
+// function login() {
   
-    let z =localStorage.getItem("users");
-    let p =JSON.parse(z);
-    let email=document.getElementById("w3lName").value
-    let password=document.getElementById("w3lSender").value
-    let  login  =p.find(x => x.email==email && x.mdp==password);
-    console.log(login);
+//     let z =localStorage.getItem("users");
+//     let p =JSON.parse(z);
+//     let email=document.getElementById("w3lName").value
+//     let password=document.getElementById("w3lSender").value
+//     let  login  =p.find(x => x.email==email && x.mdp==password);
+//     console.log(login);
     
   
-    localStorage.setItem("UserConnecté",JSON.stringify(login))
-    if (login.role == "admin"){
+//     localStorage.setItem("UserConnecté",JSON.stringify(login))
+    
+
+    
+// }
+
+function logOut() {
+    
+    localStorage.removeItem("UserConnecté");
+  
+}
+function redirect() {
+    let p=localStorage.getItem("UserConnecté");
+    let loginn = JSON.parse(p);
+    if (loginn.role == "admin"){
         window.location.replace("admin.html")
         
     }
@@ -17,12 +30,4 @@ function login() {
     window.location.replace("user.html")
         
    } 
-
-    
-}
-
-function logOut() {
-    
-    localStorage.removeItem("UserConnecté");
-  
 }
