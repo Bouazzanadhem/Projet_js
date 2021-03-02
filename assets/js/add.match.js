b883e8e8146855f64e5defe53d764fcd62c238ac
moez87 <harrabimoez87@gmail.com>
2021-03-02 15:17:10 +0100

    fix select.jsand addmatch

--- assets/js/add.match.js
+++ assets/js/add.match.js
@@ -30,7 +30,7 @@ function AddMatch(){
     
     let a = document.getElementById("inputState1").value;
     let b = document.getElementById("inputState2").value;
-
+console.log(b ,"select team2");
     let c= document.getElementById("inputState3").value;
 
 
@@ -45,5 +45,35 @@ function AddMatch(){
     listimage=[];
     console.log(g);
     window.location.replace("admin.html")
+}
+
+function SelectMatch() {
+
+ var team =["Lakers","Celtic","Warriors","Raptors","Cleveland","Bucks","Heat","Spurs","Clippers","Dallas","Chicago","Philadelphie","Nuggets","Brooklyn"];
+
+let a = document.getElementById("inputState1").value;
+// alert (a);
+
+team.forEach((x,index) => {
+    if(a==x){
+        console.log(index);
+        team.splice(index ,1);
+    }
+})
+
+
+    var elm = document.getElementById('inputState2'), // get the select
+        df = document.createDocumentFragment(); // create a document fragment to hold the options while we create them
+    for (let i = 0; i < team.length; i++) { // loop, i like 42.
+        var option = document.createElement('option'); // create the option element
+        option.value = team[i]; // set the value property
+        option.appendChild(document.createTextNode(team[i])); // set the textContent in a safe way.
+        df.appendChild(option); // append the option to the document fragment
+    }
+    elm.appendChild(df); // append the document fragment to the DOM. this is the better way rather than setting innerHTML a bunch of times (or even once with a long string)
+
+
+console.log(team);
+team=[];
 }
 
